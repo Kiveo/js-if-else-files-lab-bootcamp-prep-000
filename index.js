f16c884c8fd717a17f862f4c1595b249cc5cf01c
Maverick Tesini <mavericktesini@gmail.com>
2017-10-17 19:21:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var name = "Susan"
-var height = "74"
-if (name == "Susan") {
-  console.log("The name is correct.");
-} else {
-  console.log("The name was expected to be Susan, not " + name);
-}
+var height = "70"
+/* Redundant seeing this during tests.js file execution. Thus, commenting 'out' for now.
+*  if (name == "Susan") {
+*    console.log("The name is correct.");
+*  } else {
+*    console.log("The name was expected to be Susan, not " + name);
+*  }
+*/
 
 // Don't worry about this
 module.exports = { name, height
