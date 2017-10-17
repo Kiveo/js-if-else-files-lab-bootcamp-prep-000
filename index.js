a6750248e46b38665b6fb584d0bd4d6388cbc83e
Maverick Tesini <mavericktesini@gmail.com>
2017-10-17 19:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name == Susan) {
   console.log("The name is correct.");
+} else {
+  console.log("The name was expected to be Susan, not " + name);
 }
 
 // Don't worry about this
